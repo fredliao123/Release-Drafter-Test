514011606c674e766ab5de58cec5fd3d60a2090b
Fred Liao <fredliaobupt@gmail.com>
2021-03-01 15:06:13 +1100

    Test create new release-marker

--- app/src/main/java/com/dylange/testreleasedraft/MainActivity.kt
+++ app/src/main/java/com/dylange/testreleasedraft/MainActivity.kt
@@ -2,12 +2,13 @@ package com.dylange.testreleasedraft
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        Log.i("Create drafter", "Test")
     }
 }
