4ce3c2a63b56d117d5d74c963f57a3f59590e61f
Fred Liao <fredliaobupt@gmail.com>
2021-03-15 10:49:59 +1100

    Test delete

--- app/src/main/java/com/dylange/testreleasedraft/MainActivity.kt
+++ app/src/main/java/com/dylange/testreleasedraft/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.i("Update drafter", "Test")
+        Log.i("Test delete", "Test")
     }
 }
